3a7e872359a5b7bdeafa4aa20d4190244a18662b
Raja <raja@gmail.com>
2018-02-11 23:53:18 +0530

    Project Added

---
+++ ForEachApp/src/com/rs/client/ForEachClient.java
@@ -0,0 +1,21 @@
+package com.rs.client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForEachClient {
+	
+	public static void main(String[] args) {
+		List<String> namesList=null;
+		
+		namesList=new ArrayList<>();
+		
+		namesList.add("Lenovo");
+		namesList.add("Dell");
+		namesList.add("Hp");
+		namesList.add("Sony");
+		
+		
+	}
+
+}
