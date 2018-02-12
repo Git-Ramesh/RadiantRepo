7fb96fc0a759f5ac75c62448d950fbf6072995f3
Jaga <jaga@gmail.com>
2018-02-12 21:04:01 +0530

    Errors Solved

--- ForEachApp/src/com/rs/client/ForEachClient.java
+++ ForEachApp/src/com/rs/client/ForEachClient.java
@@ -23,7 +23,7 @@ public class ForEachClient {
 		//print the elements
 		System.out.println(namesList.get(1)+" "+namesList.get(2)+" "+namesList.get(3));
 	}
-	public static add(ArrayList<?> list){
+	public static void add(ArrayList list){
 		list.add(list);
 	}
 }
